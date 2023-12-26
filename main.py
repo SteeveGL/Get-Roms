519854df2b6e21c5ff1809e19444c20552bc36da
Steeve Gaudreault-lambert <steeveglambert@gmail.com>
2023-12-26 17:18:22 -0500

    almost done

--- main.py
+++ main.py
@@ -1,97 +1,132 @@
-import os
-import json
-import requests
-
 import customtkinter as ctk
+import json
+import os
+import tkinter as tk
+
 from tkinter import ttk
-from bs4 import BeautifulSoup
 
-config_file = "config.json"
-config_default = {
-   "host": {
-      "url": ""
-   }
-}
+from lib.roms import Links
 
-if os.path.exists(config_file):
-  with open(config_file, 'r') as file:
-    config = json.load(file)
-else:
-  with open(config_file, 'w') as file:
-    json.dump(config_default, file, indent=2)
-  config = config_default
+class App(ctk.CTk):
 
+  def __init__(self):
+    super().__init__()
 
+    self.title("Get ROMs")
+    self.geometry("800x500")
 
-def config_save():
-  try:
-      with open(config_file, 'w') as file:
-          json.dump(config, file, indent=2)
-  except IOError as e:
-      print(f"Error saving to file: {e}")
+    self.config_file = "config.json"
+    self.config = {
+      "server": {
+          "url": ""
+      },
+      "roms": {
+        "path": "./ROMs"
+      }
+    }
 
-def button_setup():
-  dialog = ctk.CTkInputDialog(text="URL:", title="ROMs source")
-  text = dialog.get_input()
-  if text:
-    config['host']['url'] = text
-    config_save()
-    panel_setup.pack_forget()
-    panel_roms.pack(expand=True, fill='both')
+    self.config_load()
 
-def links_load():
-  url = f"{config['host']['url']}/files/No-Intro/"
-  response = requests.get(url)
-  if not response.status_code == 200:
-    return None
-  
-  html_content = response.text
+    if not self.config["server"]["url"]:
+      self.dialog_setup()
+    self.roms_links = Links(self.config["server"]["url"])
 
-  soup = BeautifulSoup(html_content, 'html.parser')
-  table_body = soup.find('table', {'id': 'list'}).find('tbody')
-  link_info = [(f"{url}{link.get('href')}", link.get_text()) for link in table_body.find_all('a')]
-  link_info.pop(0)
+    treeview_frame = ctk.CTkFrame(self)
+    treeview_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    treeview_frame.pack_propagate(False)  # Prevent the frame from resizing with its content
+    treeview_frame.configure(width=50)
 
-  return link_info
+    self.treeview = ttk.Treeview(treeview_frame)
+    self.treeview_scrollbar = ttk.Scrollbar(treeview_frame, orient="vertical", command=self.treeview.yview)
+    self.treeview.configure(yscrollcommand=self.treeview_scrollbar.set)
+    self.treeview.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    self.treeview_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
+    self.treeview_items = {}
 
-ctk.set_appearance_mode("System")
-ctk.set_default_color_theme("blue")
+    for link in self.roms_links.roms_console_links():
+      title_full = link["title"]
 
-app = ctk.CTk()
-app.geometry("600x600")
+      if not " - " in title_full:
+        continue
 
-panel_setup = ctk.CTkFrame(app)
-button_setup = ctk.CTkButton(panel_setup, text="Setup", command=button_setup)
-button_setup.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+      title_split = title_full.split(" - ", 1)
+      manufacturer = title_split[0]
+      console = title_split[1].replace("/", "")
 
-panel_roms = ctk.CTkFrame(app)
-tree_view = ttk.Treeview(panel_roms)
-tree_view.pack(pady=20, expand=True, fill='both')
+      if not manufacturer in self.treeview_items:
+        self.treeview_items[manufacturer] = self.treeview.insert("", tk.END, text=manufacturer, values=("parent"))
 
-# Define columns
-tree_view['columns'] = ("Column1", "Column2")
-tree_view.column("#0", width=120, stretch='no')
-tree_view.column("Column1", anchor='center', width=120)
-tree_view.column("Column2", anchor='center', width=120)
+      self.treeview.insert(self.treeview_items[manufacturer], tk.END, text=console, values=("child", link["link"]))
 
-tree_view.heading("#0", text="Name", anchor='w')
-tree_view.heading("Column1", text="Column 1", anchor='center')
-tree_view.heading("Column2", text="Column 2", anchor='center')
+    self.treeview.bind("<<TreeviewSelect>>", self.on_tree_select)
 
-# Insert parent and child items
-parent1 = tree_view.insert("", "end", text="Parent 1", values=("Value1", "Value2"))
-parent2 = tree_view.insert("", "end", text="Parent 2", values=("Value3", "Value4"))
+    self.roms_frame = ctk.CTkFrame(self)
+    self.roms_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+    self.rom_label = ctk.CTkLabel(self.roms_frame, text="Select a console.")
+    self.rom_label.pack(anchor=tk.CENTER, pady=10)
 
-tree_view.insert(parent1, "end", text="Child 1.1", values=("Value5", "Value6"))
-tree_view.insert(parent1, "end", text="Child 1.2", values=("Value7", "Value8"))
+    self.download_button = ctk.CTkButton(self.roms_frame, text="Download", state=ctk.DISABLED, command=self.button_download)
+    self.download_button.pack(side=tk.BOTTOM, pady=20)
 
-tree_view.insert(parent2, "end", text="Child 2.1", values=("Value9", "Value10"))
+  def on_tree_select(self, event):
+    self.selected_item = self.treeview.selection()[0]
+    self.selected_parent = self.treeview.parent(self.selected_item)
+    item_text = self.treeview.item(self.selected_item, "text")
+    item = self.treeview.item(self.selected_item)
 
+    if item["values"] and item["values"][0] == "parent":
+      self.rom_label.configure(text="Select a console.")
+      self.download_button.configure(state=ctk.DISABLED)
+      return
 
-if not config['host']['url']:
-  panel_setup.pack(expand=True, fill='both')
-else:
-  panel_roms.pack(expand=True, fill='both')
+    self.links = self.roms_links.roms_links(item["values"][1])
+    self.rom_label.configure(text=f"{len(self.links)} items found.")
+    self.download_button.configure(state=ctk.NORMAL)
 
-app.mainloop()
+  def button_download(self):
+    progress_label = ctk.CTkLabel(self.roms_frame)
+    progress_label.pack(anchor=tk.CENTER, pady=10)
+    progress = ctk.CTkProgressBar(self.roms_frame, orientation="horizontal")
+    progress.pack(anchor=tk.CENTER, pady=20)
+
+    done = 0
+
+    for link in self.links:
+      manufacturer = self.treeview.item(self.selected_parent, "text")
+      console = self.treeview.item(self.selected_item, "text")
+      download_path = f"{self.config['roms']['path']}/{manufacturer}/{console}/{link['title']}"
+
+      progress_label.configure(text=link['title'])
+      self.roms_links.rom_download(link["link"], download_path)
+
+      done += 1
+      progress.set((done / len(self.links)) * 100)
+
+    progress_label.destroy()
+    progress.destroy()
+
+  def dialog_setup(self):
+    dialog = ctk.CTkInputDialog(text="Server URL:", title="ROMs source")
+    self.config['server']['url'] = dialog.get_input()
+    self.config_save()
+
+  def config_load(self):
+    if os.path.exists(self.config_file):
+      with open(self.config_file, 'r') as file:
+        self.config = json.load(file)
+    else:
+      with open(self.config_file, 'w') as file:
+        json.dump(self.config, file, indent=2)
+        self.dialog_setup()
+
+  def config_save(self):
+    try:
+        with open(self.config_file, 'w') as file:
+            json.dump(self.config, file, indent=2)
+    except IOError as e:
+        print(f"Error saving to file: {e}")
+
+if __name__ == "__main__":
+  app = App()
+  app.mainloop()
